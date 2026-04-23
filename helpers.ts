1bc25c06bc6652debec0497689df9fe8491b435f
trickbdnetwork2 <trickbd.network2@gmail.com>
2026-04-23 15:20:11 +0600

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T09:20:03.698Z
-Commit: Add multi-language support
+Timestamp: 2026-04-23T09:20:10.435Z
+Commit: Resolve null pointer exception in user service
