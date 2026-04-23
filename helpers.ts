2ac15f6cddf270f4786f83d6611df50993b989ed
trickbdnetwork2 <trickbd.network2@gmail.com>
2026-04-23 15:19:42 +0600

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T09:19:31.962Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-23T09:19:41.738Z
+Commit: Fix memory leak in event handlers
