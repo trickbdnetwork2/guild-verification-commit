6928457523ac608eb5bbbafe0f042253ec84ac37
trickbdnetwork2 <trickbd.network2@gmail.com>
2026-04-23 15:20:39 +0600

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T09:20:10.435Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-23T09:20:39.323Z
+Commit: Add dark mode support
